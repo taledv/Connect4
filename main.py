b4d2e5760a081d1ed15e34e3fdf45c0bff0ac3a6
taledv <taledvabsky@gmail.com>
2020-11-19 17:00:44 +0200

    when game is done, back to intro screen to play again

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ width = COL_COUNT*SQUARESIZE
 height = (ROW_COUNT+1)*SQUARESIZE
 size = (width, height)
 
-board = create_board()
-turn = np.random.randint(2)  # Random initial player
-
 Player = 1
 AI = 2
 
-pygame.init()  # Init pygame
-pygame.display.set_mode(size)  # Set window size
-screen = pygame.display.set_mode(size)  # Init screen (surface)
+while 1:
+    board = create_board()
+    turn = np.random.randint(2)  # Random initial player
 
-game_mode, ai_lvl = intro_screen(screen, board)
+    pygame.init()  # Init pygame
+    pygame.display.set_mode(size)  # Set window size
+    screen = pygame.display.set_mode(size)  # Init screen (surface)
 
+    game_mode, ai_lvl = intro_screen(screen, board)
 
-if game_mode == Player:
-    p_vs_p(screen, board, turn)
-else:
-    p_vs_ai(screen, board, turn, ai_lvl)
+    if game_mode == Player:
+        p_vs_p(screen, board, turn)
+    else:
+        p_vs_ai(screen, board, turn, ai_lvl)
